f600f0bd012d2c403d4c5514936eef7df66dfad4
Argeu Alcantara <a.argeu@gmail.com>
2015-02-26 16:43:42 -0300

    Refactoring methods

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ list_statuses = rtc_client.getProjectAreaStatuses(list_types)
 #lastModified is a date that you pass as a string like 'yyyy-mm-dd hh24:mi:ss'
 #if it's none it'll search for workitems with identifier greater or equals to 1
 lastModified = None
-
+lastModified = '2015-02-15 15:15:15'
 for project_area in list_project_areas.values():
 	list_workitems = rtc_client.getProjectAreaWorkitems(project_area, lastModified)
 	print ('%s -> %i' %(project_area['Title'], len(list_workitems)))
